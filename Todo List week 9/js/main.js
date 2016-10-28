e2a5c67885a6e779ba3c221d0a854be46ad4278c
mocotonioaz <mocotonioaz@gmail.com>
2016-10-28 16:32:14 -0700

    Todo List week 9

--- Todo List week 9/js/main.js
+++ Todo List week 9/js/main.js
@@ -4,15 +4,20 @@ function todoList() {
 
 	var userNameText = document.createTextNode("Username: " + username)
 	var commentText = document.createTextNode("Comment: " + comment)
-
+     
 	var newListItem = document.createElement("LI")
 	newListItem.className = "list-group-item"
 	var newUserName = document.createElement("H3")
 	newUserName.appendChild(userNameText)
 	var newComment = document.createElement("P")
 	newComment.appendChild(commentText)
+	var newImg = document.createElement('img')
+    newImg.setAttribute('src', "http://vignette3.wikia.nocookie.net/nintendo/images/f/f4/The_Mario_Bros..jpeg/revision/latest?cb=20140221224337&path-prefix=en"),
+    
 
+   
 	newListItem.appendChild(newUserName)
 	newListItem.appendChild(newComment)
+	newListItem.appendChild(newImg)
 	document.getElementById("commentList").appendChild(newListItem)
 }
